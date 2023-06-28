4e8ef5d6d3a41f8d7f437433fb6ab97aecb4990f
Naiara Pantuza <naiarapantuza@gmail.com>
2023-06-28 10:40:20 -0300

    Initial Commit

---
+++ sol-viagens/sol_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  sol_viagensApp.swift
+//  sol-viagens
+//
+//  Created by Naiara de Almeida Pantuza on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct sol_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
