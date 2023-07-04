549f1f1a495b2b9c6cb43d6f894168e63b41bbb9
Naiara Pantuza <naiarapantuza@gmail.com>
2023-07-04 12:16:54 -0300

    ajustes célula

--- sol-viagens/Views/TripCellView.swift
+++ sol-viagens/Views/TripCellView.swift
@@ -11,16 +11,24 @@ struct TripCellView: View {
   
   var trip: Trip
   
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  
   var body: some View {
     VStack (alignment: .leading) {
       Text(trip.title)
+        .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
+      
       Image(trip.image)
         .resizable()
-        .frame(height: 125)
+        .aspectRatio(contentMode: .fill)
+        .frame(height: self.horizontalSizeClass == .compact ? 125: 200)
+        .clipped()
       HStack {
         Text(trip.numberOfDays)
+          .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
         Spacer()
         Text(trip.value)
+          .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
       }
     }
   }
